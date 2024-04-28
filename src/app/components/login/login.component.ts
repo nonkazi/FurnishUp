23330eb636ba4ed55d94dab98fc8c747a25c4193
mpho2024 <mpho.tsatsi01@gmail.com>
2024-04-28 20:54:52 +0200

    mmm

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  public loginForm !:FormGroup
+  constructor(private formbuilder:FormBuilder, private http: HttpClient, private router:Router){}
 
-}
+  ngOnInit():void
+  {
+    this.loginForm = this.formbuilder.group({
+      email: [''],
+      password: ['', Validators.required]
+    })}
+
+    login()
+    {
+      this.http.get<any>("http://localhost:3000/signupUsersList").subscribe(res=>{
+        const user = res.find((details:any)=>
+        {
+          return details.email === this.loginForm.value.email && details.password === this.loginForm.value.password;
+        });
+        console.log(this.loginForm);
+        
+        if(user)
+        {
+          alert('Successfully Logged in');
+          this.loginForm.reset();
+          this.router.navigate(["/home"])
+        }
+        else
+        {
+          alert("User not found")
+        }
+      },err=>
+      {
+        alert("Something went wrong");
+      })
+    
+}}
