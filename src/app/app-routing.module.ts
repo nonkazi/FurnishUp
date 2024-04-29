ed7f0bf9493d39c5a1897bf3fd322ce6439701d0
sihle1996 <mofokengsihle63@gmail.com>
2024-04-29 09:38:10 +0200

    testing router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [{path:'home',component:HomeComponent},
 {path:'products',component:ProductsComponent},
-{path:'checkout',component:CheckoutComponent}
+{path:'checkout',component:CheckoutComponent},
+{path:'login',component:LoginComponent},
+{path:'register',component:RegisterComponent}
 ];
 
 @NgModule({
