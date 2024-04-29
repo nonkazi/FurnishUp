ec6b6a5821797d52e7863e6e4f1bd628d36c0748
sihle1996 <mofokengsihle63@gmail.com>
2024-04-29 11:15:38 +0200

    routerlink

--- src/app/Carts/cart.service.ts
+++ src/app/Carts/cart.service.ts
@@ -2,39 +2,36 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-  
-  private cart: any[] = []
+  private cart: any[] = [];
 
-  private cartItemCount = new BehaviorSubject<number>(0) 
-  
+  private cartItemCount = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  constructor() {}
 
-  getCart(){
-    return this.cart
+  getCart() {
+    return this.cart;
   }
 
-  getCartItemCount(){
-    return this.cartItemCount
+  getCartItemCount() {
+    return this.cartItemCount;
   }
 
-  addTocart(products:any){
+  addTocart(products: any) {
     let added = false;
-      this.cart.forEach((item)=> {
-        item.id === products.id
-        item.quantity += 1;
-        added = !added;
-      });
-       
+    this.cart.forEach((item) => {
+      item.id === products.id;
+      item.quantity += 1;
+      added = !added;
+    });
+
     if (!added) {
       products.quantity = 1;
       this.cart.push(products);
     }
-    console.log(this.cart)
+
     this.cartItemCount.next(this.cartItemCount.value + 1);
   }
- 
 }
