de1230c74b6e0d4d5bae6748e41874eaec24a800
mpho2024 <khanyatsatsi@gmail.com>
2024-04-29 16:47:53 +0200

    Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,31 +2,25 @@ import { Component, NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { loginComponent} from './components/login/login.component';
+import { loginComponent } from './components/login/login.component';
 import { registerComponent } from './components/register/register.component';
 import { CartComponent } from './components/cart/cart.component';
-import { CheckoutComponent } from './components/checkout/checkout.component'
+import { CheckoutComponent } from './components/checkout/checkout.component';
 import { RouterModule, Routes } from '@angular/router';
 
-
-
-
-const Routes: Routes = [{path:'home',component:HomeComponent},
-{ path: '', redirectTo: '/home', pathMatch: 'full' },
-{path:'',component: HomeComponent},
-{path:'home',component: HomeComponent},
-{path:'products',component:ProductsComponent},
-{path:'checkout',component:CheckoutComponent},
-  {path:'productdetais',component: ProductDetailComponent},
-  {path:'cart',component: CartComponent},
-  {path:'login',component: loginComponent},
-  {path:'register',component: registerComponent},
-
-
-]
+const Routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'productdetails', component: ProductDetailComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'login', component: loginComponent },
+  { path: 'register', component: registerComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(Routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
